e0d9ed16a12d30d537caf3d7c65028841384afa7
kriss-spy <3574725417@qq.com>
2025-06-24 23:54:27 -0700

    static grid size by js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const gridSize = 16;
+
+const grid = document.querySelector("#grid");
+for (let i = 0; i < gridSize; i++) {
+  let item = document.createElement("div");
+  item.classList.add("item");
+  grid.appendChild(item);
+}
+
 const items = document.querySelectorAll(".item");
 items.forEach((item) => {
   item.addEventListener("mouseover", () => {
